7db9e780e09eb254f58ead7e6f91e7db95032f3c
Joaquín Méndez <joaquin.enmt@gmail.com>
2021-06-21 16:55:01 -0400

    Primer commit

---
+++ Loteria/Loteria/index.aspx.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Loteria
+{
+    public partial class index : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btn_Click(object sender, EventArgs e)
+        {
+            byte[] sorteo = new byte[6];
+            Random rdm = new Random();
+            rdm.NextBytes(sorteo);
+
+            foreach (var item in sorteo)
+            {
+                
+            }
+        }
+    }
+}
